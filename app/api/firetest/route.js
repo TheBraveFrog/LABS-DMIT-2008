60225a1d0887605bcd7a470efb243e969b721d0f
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-08 16:01:33 -0700

    changes

--- app/api/firetest/route.js
+++ app/api/firetest/route.js
@@ -1,6 +1,6 @@
 import {db} from '@/lib/firebase/firebaseConfig'
 async function GET (req, {params}) {
-    const dbRef =  db.ref('/users')
+    const dbRef =  db.ref('/records')
     const dataSnapshot = await dbRef.once('value')
     const rtdData = dataSnapshot.val()
     return Response.json(rtdData)
