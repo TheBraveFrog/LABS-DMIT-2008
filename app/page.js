bb155bbc3b259726c5127e23fef829fc99ded228
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-08 17:16:56 -0700

    page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,34 @@
+
+import { PageHeader } from "../../components/header/PageHeader"
+import { getRecords } from "@/lib/firebase/getEmployees";
  
-export default function Home() {
-  return (
-     <main>
-       ...start
+
+// Link element to create a dynamic route.
+
+ 
+
+ async function RecordsPage (  ) {
+   const payload = await getRecords();
+    const keys =  Object.keys(payload)
+    const values = Object.values(payload)
+    const entries = Object.entries(payload)
+ 
+
+  
+
+   return(
+    <>
+    <PageHeader title="All Records Display"/>
+    <main className=" min-h-screen py-24">
+       {
+          values.map(item=> <Card  key={item.uid} {...item}/>)
+       }
      </main>
-  )
+    </>
+   )
+  
+  
 }
+
+
+export default RecordsPage
